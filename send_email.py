83cc479dfc4aeee3689116c5af03dc5a58266fdf
erikv05 <erikv05@uw.edu>
2024-02-26 23:54:00 -0800

    initial commit

---
+++ send_email.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+import json
+
+custom_data = pd.read_csv("oversea_test_data.csv")
+
+for i in range(custom_data.shape[0]):
+    emails = []
+    custom_cols = []
+    for j in range(7, custom_data.shape[1]):
+        col = {}
+        col["attribute"] = custom_data.columns[j]
+        col["description"] = custom_data.iloc[i,j]
+        custom_cols.append(col)
+    email = {
+        'companyDescription': custom_data.iloc[i]["Company Description"],
+        'productDescription': custom_data.iloc[i]["Product Description"],
+        'companyName': custom_data.iloc[i]["Company Name"],
+        'customerAge': custom_data.iloc[i].astype("string")["Customer Age"],
+        'marketerName': custom_data.iloc[i]["Marketer Name"],
+        'customerName': custom_data.iloc[i]["Customer Name"],
+        'email': custom_data.iloc[i]["Email"],
+        'customAttributes': custom_cols
+        }
+    emails.append(email)
+
+url = "http://127.0.0.1:8080/generate-email"
+
+for email in emails:
+    res = requests.post(url, json = email)
+    print(res.text)
---
+++ server/app.py
@@ -0,0 +1,83 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, session, jsonify, send_file, make_response, g, render_template
+from flask_cors import CORS, cross_origin
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+from google.cloud import secretmanager_v1beta1 as secretmanager
+from sendgrid import SendGridAPIClient
+from openai import OpenAI
+import firebase_admin
+from firebase_admin import credentials
+
+
+#set up flask app
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../frontend/build',
+    template_folder='../frontend/build'
+)
+#load environment variables
+load_dotenv()
+client = OpenAI(api_key=os.getenv("GPT-API-KEY"))
+
+#development branch
+prod = os.environ.get('PROD')
+allowed_origins = [
+    "https://oversealabs.io",
+] if prod == "PRODUCTION" else ["http://localhost:3000"]
+CORS(app, resources={r"/*": {"origins": "*"}})
+mail = Mail(app)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+#setup domain
+YOUR_DOMAIN = 'https://oversealabs.io' if prod == "PRODUCTION" else 'http://localhost:3000'
+#FIXME: place service account in secret files location and load it from there
+# cred = credentials.Certificate("serviceAccountKey.json")
+# firebase_admin.initialize_app(cred)
+
+
+
+#serve all the react files
+@app.errorhandler(404)   
+def not_found(e):   
+  return render_template("index.html")
+
+@app.route('/generate-email', methods=['POST'])
+def generate_email():
+    try:
+        # Get the JSON data from the request
+        data = request.json
+
+        # Access individual fields from the data
+        marketer_name = data.get('marketerName', '')
+        product_description = data.get('productDescription', '')
+        company_name = data.get('companyName', '')
+        company_description = data.get('companyDescription', '')
+        customer_name = data.get('customerName', '')
+        customer_age = data.get('customerAge', '')
+        custom_attributes = data.get('customAttributes', [])
+
+        prompt = f"""Write a persoanlized marketing email for {company_name} with the following company description: {company_description}.\nThe product you are selling has this description: {product_description}. The customer you write to is {customer_name}, a {customer_age}-year-old.\nYou may use any or all of the following customer info: """
+        for att in custom_attributes:
+            prompt += att['attribute'] + ': ' + att['description'] + ',\n'
+        prompt += "and remember to make the email as personal as possible.\nThe email must be less than 200 words."
+
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": f"You are a skilled email writer on the marketing team named {marketer_name} working to write simple and easy-to-read personalized emails."},
+                {"role": "user", "content": prompt}
+            ],
+        )
+        return jsonify({'email': completion.choices[0].message.content}), 200
+
+    except Exception as e:
+        # Handle exceptions if any
+        print('Error:', str(e))
+        return jsonify({'error': f'An error occurred'}), 500
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
+
